8635882a860476bf35f220ae968cd7d0e998d77a
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-04-13 18:25:26 -0300

    Time: 8 ms (53.26%) | Memory: 18.3 MB (69.42%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -4,32 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Time O(Nˆ2)
+# Space O(N)
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        cumulative_paths = {0:1}
         def dfs(root, cumulative_path, targetSum):
             if root is None: return 0
-            cur_paths = 0
-            for i in range(0, len(cumulative_path)):
-                val = cumulative_path[i]
-                if val + root.val == targetSum:
-                    cur_paths += 1
-                cumulative_path[i] += root.val
-            if root.val == targetSum:
-                cur_paths += 1
+            cumulative_path += root.val
+            paths = cumulative_paths.get(cumulative_path - targetSum, 0)
 
-            cumulative_path.append(root.val)
+            cumulative_paths[cumulative_path] = cumulative_paths.get(cumulative_path, 0) + 1
 
             pos_left = dfs(root.left, cumulative_path, targetSum)
             pos_right = dfs(root.right, cumulative_path, targetSum)
 
-            cumulative_path.pop(-1)
+            cumulative_paths[cumulative_path] -= 1
 
-            for i in range(0, len(cumulative_path)):
-                cumulative_path[i] -= root.val
- 
-
-            return pos_left + pos_right + cur_paths
-        return dfs(root, [], targetSum)
+            return pos_left + pos_right + paths
+        return dfs(root, 0, targetSum)
             
 
 
