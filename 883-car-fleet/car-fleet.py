a8ecb97c9d65d54e215e7aaa5a454afa856ed81e
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-04-14 21:59:00 -0300

    Time: 213 ms (32.37%) | Memory: 43.4 MB (7.17%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -14,13 +14,12 @@ class Solution:
         for i in range(len(index_pos_sorted)):
             time.append((target - index_pos_sorted[i][1]) / sorted_speed[i])
         
-        fleet_count = 1
-
-        for i in range(len(time) -1, 0, -1):
-            if time[i-1] > time[i]:
-                fleet_count += 1
-            else:
-                time[i-1] = time[i]
         
-        return fleet_count
+        stack = []
+        for i in range(len(time) -1, -1, -1):
+            stack.append(time[i])
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop() 
+        
+        return len(stack)
    
