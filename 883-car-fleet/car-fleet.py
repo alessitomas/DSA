ddcee0040daa55b02d0d7e39c0db9b8e8b5e847e
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-04-14 21:22:45 -0300

    Time: 171 ms (51.68%) | Memory: 43.7 MB (6.09%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,26 +1,26 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         index_pos_sorted = sorted(enumerate(position), key = lambda x : x[1])
-        print("index_pos_sorted", index_pos_sorted)
+    
         sorted_speed = []
         for index, _ in index_pos_sorted:
             sorted_speed.append(speed[index])
         
-        print("sorted_speed.append", sorted_speed)
+    
         # position
         # speed
 
         time = []
         for i in range(len(index_pos_sorted)):
             time.append((target - index_pos_sorted[i][1]) / sorted_speed[i])
-        print("time", time)
+        
         fleet_count = 1
 
         for i in range(len(time) -1, 0, -1):
-            if time[i-1] <= time[i]:
-                time[i-1] = time[i]
-            else:
+            if time[i-1] > time[i]:
                 fleet_count += 1
+            else:
+                time[i-1] = time[i]
         
         return fleet_count
    
