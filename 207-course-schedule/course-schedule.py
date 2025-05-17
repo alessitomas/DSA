3596ec4aaa8f7c615fc47b55226cf9926da4a3df
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-05-17 11:41:52 -0300

    Time: 11 ms (15.02%) | Memory: 19.5 MB (22.06%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,51 +1,61 @@
+# 3 ways of representing a graph
+
+# adjancency list
+# matrix
+# objects and pointers
+
 class Solution:
-    class Graph:
-        def __init__(self, val):
-            self.val = val
-            self.adjacent = []
-    
-    def dfs_exploration(self, cur_node, visited, seen):
-        if cur_node in visited:
-            return False
-        if cur_node in seen:
-            return True
-        visited.add(cur_node)
-        
-        
-        for req in cur_node.adjacent:
-            if not self.dfs_exploration(req, visited, seen):
-                return False
-        visited.remove(cur_node)
-        seen.add(cur_node)
-        return True
-
-
+    class GraphNode:
+        def __init__(self):
+            self.next = []
     
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        nodes = {}
-        for course_req in prerequisites:
-            course = course_req[0]
-            req = course_req[1]
-
-            if not course in nodes:
-                nodes[course] = self.Graph(course)
-            if not req in nodes:
-                nodes[req] = self.Graph(req)
-
-            nodes[course].adjacent.append(nodes[req])
+        index_to_node = {}
         
-        seen = set()
-        
-        for node in nodes.values():
-            if not node in seen:
-                if not self.dfs_exploration(node, set(), seen):
-                    return False
-
-        return True
-
-
-        
-
+        for course_index, req_index in prerequisites:
+            # if course node is not yet, initialized
+            if course_index not in index_to_node:
+                index_to_node[course_index] = self.GraphNode()
+            # if req node is not yet, initialized
+            if req_index not in index_to_node:
+                index_to_node[req_index] = self.GraphNode()
             
+            # lets connect the node through pointers
+            index_to_node[course_index].next.append(index_to_node[req_index])
+
+        def dfs_cycle_detection(cur_node, stack_visited, completed_courses):
+            # cycle base case
+            if cur_node in stack_visited:
+                return True
+            # was alredy marked as completed base case
+            if cur_node in completed_courses:
+                return False
+            
+            stack_visited.add(cur_node)
+            
+            # all requisites
+            for req in cur_node.next:
+                has_cycle = dfs_cycle_detection(req, stack_visited, completed_courses)
+                
+                if has_cycle:
+                    return True
+
+            # no req has cycle
+            stack_visited.remove(cur_node)
+            completed_courses.add(cur_node)
+            return False
+        
+        completed_courses = set()
+        for node in index_to_node.values():
+            has_cycle = dfs_cycle_detection(node, set(), completed_courses)
+            if has_cycle:
+                return False
+        
+        return True
+        
+
+
+
+
 
         
