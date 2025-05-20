ebc0d1fb1444cae55261908094d9965add05f9a5
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-05-20 09:35:56 -0300

    Time: 25 ms (48.77%) | Memory: 18.6 MB (32.80%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -2,28 +2,25 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         map_visited_cells = set()
         
-        def calculate_area(postion, visited):
-            i, j = postion
-            # out of the map
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
-                return 0
-            # water or visited case
-            if grid[i][j] == 0 or (i, j) in visited:
-                return 0
-            
-            area = 1
-            visited.add((i, j))
-            map_visited_cells.add((i, j))
-
-            for i_mv, j_mv in [(-1,0), (0,1), (1,0), (0, -1)]:
-                new_i = i + i_mv
-                new_j = j + j_mv
-                area += calculate_area((new_i, new_j), visited)
+        def calculate_area(position):
+            stack = [position]
+            area = 0
+            while len(stack) > 0:
+                i, j = stack.pop(-1)
+                if (i, j) in map_visited_cells:
+                    continue
+                area += 1
+                map_visited_cells.add((i, j))
+                for i_mv, j_mv in [(-1,0), (0,1), (1,0), (0, -1)]:
+                    new_i = i + i_mv
+                    new_j = j + j_mv
+                    if (new_i >= 0 and new_i  < len(grid) 
+                        and new_j  >= 0 and new_j  < len(grid[0]) 
+                        and grid[new_i][new_j] != 0 
+                        and (new_i, new_j) not in map_visited_cells):
+                            stack.append((new_i,new_j))
             
             return area
-
-
-
 
         max_area = 0
         for i in range(len(grid)):
@@ -31,7 +28,7 @@ class Solution:
                 cell_value = grid[i][j]
                 # expand
                 if cell_value == 1 and (i,j) not in map_visited_cells:
-                    new_area = calculate_area((i,j), set())
+                    new_area = calculate_area((i,j))
                     max_area = max(max_area, new_area)
 
         return max_area
