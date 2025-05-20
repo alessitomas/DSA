f204ce0e23d2c9745c36c14d57ab314e087fef47
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-05-20 09:37:38 -0300

    Time: 27 ms (45.64%) | Memory: 18.4 MB (54.40%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -2,15 +2,15 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         map_visited_cells = set()
         
+        
         def calculate_area(position):
             stack = [position]
+            map_visited_cells.add(position)
             area = 0
+
             while len(stack) > 0:
                 i, j = stack.pop(-1)
-                if (i, j) in map_visited_cells:
-                    continue
-                area += 1
-                map_visited_cells.add((i, j))
+                area += 1    
                 for i_mv, j_mv in [(-1,0), (0,1), (1,0), (0, -1)]:
                     new_i = i + i_mv
                     new_j = j + j_mv
@@ -19,6 +19,7 @@ class Solution:
                         and grid[new_i][new_j] != 0 
                         and (new_i, new_j) not in map_visited_cells):
                             stack.append((new_i,new_j))
+                            map_visited_cells.add((new_i,new_j))
             
             return area
 
