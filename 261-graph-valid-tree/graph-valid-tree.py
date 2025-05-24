33866c242d6ce57745a27a9a41829420a0f844d7
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-05-24 08:50:17 -0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (36.55%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,56 +1,32 @@
-# tree does no have cycle
-# parent is just connected to its directs sons
-# each son only have one parent
-# there are no connections between nodes in the same level
-# it must be a connected graph
+from collections import defaultdict
 
-from collections import deque
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        class GraphNode:
-            def __init__(self, val):
-                self.val = val
-                self.connections = []
-        if n == 1:
-            return True
-        node_map = {}
-        # creating of Graph Nodes
-        for nd1, nd2 in edges:
-            if nd1 not in node_map:
-                node_map[nd1] = GraphNode(nd1)
-            if nd2 not in node_map:
-                node_map[nd2] = GraphNode(nd2)
+        if len(edges) != n - 1: return False
+        if n == 0: return True
+
+        graph_adjc_list = defaultdict(list)
+        
+        for node_a, node_b in edges:
+            graph_adjc_list[node_a].append(node_b)
+            graph_adjc_list[node_b].append(node_a)
             
-            node_map[nd1].connections.append(node_map[nd2])
-            node_map[nd2].connections.append(node_map[nd1])
+        visited = set()
         
-        def bfs_no_cycle(node, visited):
-            queue = deque()
-            queue.append((node, None))
-            visited.add(node)
+        def dfs_detecting_cycle(cur_node, parent):
+            if cur_node in visited:
+                return True
+            
+            visited.add(cur_node)
 
-            while len(queue) > 0:
-                cur_node, parent_node = queue.popleft()
+            for neigh in graph_adjc_list[cur_node]:
+                if neigh == parent:
+                    continue 
 
-                for connection in cur_node.connections:
-                    if connection != parent_node:
-                        # cycle case
-                        if connection in visited:
-                            return False
-                        queue.append((connection, cur_node))
-                        visited.add(connection)
-
-            return True
-
-
-        if 0 not in node_map:
+                if dfs_detecting_cycle(neigh, cur_node):
+                        return True
+            
             return False
-        visited_nodes = set()
-        ## bfs no cycle
-        if bfs_no_cycle(node_map[0], visited_nodes) and len(visited_nodes) == n:
-            return True
-        return False
 
-        
 
-        
+        return not dfs_detecting_cycle(0, None) and len(visited) == n
