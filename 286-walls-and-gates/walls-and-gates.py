9e79596f2b0fac2340f66da2a83a3169cf9ff822
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-05-25 22:05:18 -0300

    Time: 43 ms (58.35%) | Memory: 22.1 MB (40.69%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -9,48 +9,46 @@ class Solution:
         # 2. For each gate do a BFS traversal over the matrix, update the cell value if the # distance to the gate is less then the value of the cell
 
         # 1. Store the position of each gate
-        # space (mn)
+        
+        # time (mn)
         gates_position = []
+        INF = 2**31 - 1 
         
         # time (mn)
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
-                    gates_position.append((i,j))
+                    gates_position.append((i,j,0))
 
-
-        def bfs_traversal(gate_position):
+        # time (mn)
+        def bfs_traversal(gates_position):
             def in_bounds(i, j):
                 if (i < len(rooms) and i >= 0 
                     and j < len(rooms[0]) and j >= 0):
                     return True
                 return False
 
-            visited = set()
-            visited.add(gate_position)
             # i, j, gate_dist 
-            queue = deque()
-            queue.append((gate_position[0], gate_position[1], 0))
+            # SPACE (mn)
+            queue = deque(gates_position)            
 
             while len(queue) > 0:
                 cur_cell = queue.popleft()
                 i, j, dist = cur_cell
-                rooms[i][j] = dist
+                
                 # up, right, down, left 
                 for mv_i, mv_j in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
                     new_i = i + mv_i 
                     new_j = j + mv_j
                     if (not in_bounds(new_i, new_j) 
-                        or (new_i, new_j) in visited
-                        or rooms[new_i][new_j] < dist + 1 
-                        or rooms[new_i][new_j] == -1):
+                        or rooms[new_i][new_j] != INF):
                         continue
                     
                     queue.append((new_i, new_j, dist+1))
-                    visited.add((new_i, new_j))
+                    rooms[new_i][new_j] = dist + 1
+                
 
-        # time O (V * G) 
-        for gate_position in gates_position:
-            bfs_traversal(gate_position)
+   
+        bfs_traversal(gates_position)
         
         return rooms
