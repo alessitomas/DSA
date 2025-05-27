8a28f619880e77ebb2af4ca0298a888fbb9d8672
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-05-27 20:02:19 -0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (48.28%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -22,7 +22,18 @@
 # if there is a relation they must be connected
 
 # there are relations between division
+
 # A / B = c
+
+# A = B * c
+
+# A * C-> B
+# A = b * 2
+
+# B * C = A 
+
+# B / C -> A
+
 
 # A -> B is 1 / c 
 # B -> A is 1 * c 
@@ -33,28 +44,22 @@
 
 # To calculate A / C, it is neede to traverse the relations graph from A to C calculating # the amount. If there is no connection return -1.
 
-
+# Space Complexity: (E + V + Q)
+# Time Complexity: (Q * (E + V))
+from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        class GraphNode:
-            def __init__(self):
-                self.neighboors = []
-                self.weights = []
+        
+        graph = defaultdict(defaultdict)
         
         node_map = {}
         
         for (num, div), res in zip(equations, values):
-            if num not in node_map:
-                node_map[num] = GraphNode()
-            if div not in node_map:
-                node_map[div] = GraphNode()
             # num -> div
-            node_map[num].neighboors.append(node_map[div])
-            node_map[num].weights.append(res)
-            # num -> div
-            node_map[div].neighboors.append(node_map[num])
-            node_map[div].weights.append(1/res)
-        
+            graph[num][div] = res
+            # div -> num
+            graph[div][num] = 1/res
+            
         results = []
         
         def dfs_traversal(cur_node, target_node, cur_amount, visited, NO_RESULT):
@@ -65,7 +70,8 @@ class Solution:
             
             visited.add(cur_node)
             
-            for neigh, weight in zip(cur_node.neighboors, cur_node.weights):
+            for neigh in graph[cur_node]:
+                weight = graph[cur_node][neigh]
                 res = dfs_traversal(neigh, target_node, cur_amount * weight, visited, NO_RESULT)
                 if res != NO_RESULT:
                     return res
@@ -74,14 +80,13 @@ class Solution:
 
         NO_RESULT = None
         for num, div in queries:
-            if num not in node_map or div not in node_map:
-                results.append(-1)
+            if num not in graph or div not in graph:
+                res = -1
             else:
-                res = dfs_traversal(node_map[num], node_map[div], 1, set(), NO_RESULT)
-                if res == NO_RESULT:
-                    results.append(-1)
-                else:
-                    results.append(res)
+                res = dfs_traversal(num, div, 1, set(), NO_RESULT)
+                res = res if res is not NO_RESULT else -1
+            
+            results.append(res)
 
         return results
 
