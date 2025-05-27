3df1325e655a23d8716ca5ee0dad0755fa2fcf00
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-05-26 21:33:11 -0300

    Time: 43 ms (33.13%) | Memory: 19.4 MB (46.03%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,58 +1,77 @@
+from collections import deque
 class Solution:
-    # Time O ( mˆ2 nˆ2)
+    
+    # cell to water
+    # i, j -> ni, nj
+    # height[i][j] >= height[ni][nj], in the lower ones I can use the work I've already done.
+
+    # water to cell
+    # i, j -> ni, nj
+    # height[i][j] <= height[ni][nj], all nodes traversed can have their state marked.
+
+
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # for each cell, do a BFS traversal over the graph if marked the condition  
-        # return True
-        # add all initial positions that returned true to the answer
-        from collections import deque
-        def bfs_traversal(i, j, visited, pacific, atlantic, heights):      
+        if heights is None:
+            return []
+        
+        if len(heights) == 0:
+            return []
+        
+        if len(heights[0]) == 0:
+            return []
+
+        def bfs(i, j, visited):
+            if (i,j) in visited: return
             queue = deque([(i,j)])
             visited.add((i,j))
             
-            def in_bounds(i, j):
-                if (i >= 0 and i < len(heights) 
-                    and j >= 0 and j < len(heights[0])):
+            def in_bounds(i,j):
+                if i >=0 and i < len(heights) and j >= 0 and j < len(heights[0]):
                     return True
                 return False
-
-            while len(queue) > 0:
-                i, j = queue.popleft()
-                for mv_i, mv_j in [(-1,0), (0,1), (1,0), (0,-1)]:
-                    new_i = i + mv_i
-                    new_j = j + mv_j
-                    
-                    if (not in_bounds(new_i, new_j) 
-                        or heights[new_i][new_j] > heights[i][j]
-                        or (new_i, new_j) in visited):
-                        continue
-
-                    # update pacific if not true
-                    pacific = pacific or (new_i == 0 or new_j == 0)
-                    atlantic = atlantic or ((new_i == len(heights) - 1) or new_j == (len(heights[0]) - 1))
-
-                    if pacific and atlantic:
-                        return True
-                    
-                    queue.append((new_i, new_j))
-                    visited.add((new_i, new_j))
             
-            return False
+            while len(queue) > 0:
+                cur_pos = queue.popleft()
+                for mv_i, mv_j in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+                    neigh_pos = (cur_pos[0] + mv_i, cur_pos[1] + mv_j)
+                    
+                    if (not in_bounds(neigh_pos[0], neigh_pos[1]) 
+                        or neigh_pos in visited 
+                        or heights[neigh_pos[0]][neigh_pos[1]] < heights[cur_pos[0]][cur_pos[1]]):
+                        continue
+                    
+                    queue.append(neigh_pos)
+                    visited.add(neigh_pos)
 
 
-        pacific_atlantic = [] 
-        
+
+        pacific_visited = set()
+        atlantic_visited = set()
+
+        # height
         for i in range(len(heights)):
-            for j in range(len(heights[0])):
-                
-                # initially connected to pacific
-                pacific = i == 0 or j == 0
-                # initially connected to atlantic
-                atlantic = (i == len(heights) - 1) or (j == len(heights[0]) - 1)
+            # pacific
+            bfs(i, 0, pacific_visited)
+            # atlantic
+            bfs(i, len(heights[0])-1, atlantic_visited)
 
-                if (pacific and atlantic) or bfs_traversal(i, j, set(), pacific, atlantic, heights):
-                    pacific_atlantic.append([i,j])
+        # width
+        for i in range(len(heights[0])):
+            # pacific
+            bfs(0, i, pacific_visited)
+            # atlantic
+            bfs(len(heights)-1, i, atlantic_visited)
+        
+        positions = []
 
-        return pacific_atlantic
+        for pacific_pos in pacific_visited:
+            if pacific_pos in atlantic_visited:
+                positions.append([pacific_pos[0], pacific_pos[1]])
+
+        return positions
+
+
+
 
 
 
