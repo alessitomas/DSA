1a2b5fd236dbd8aa1a3495627ba2f10725fa8437
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-06-01 14:53:58 -0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.17%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,51 +1,27 @@
-# ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
-
-
-# two solutions
-# use a stack to traverse left to right and back track 2 whenever a operations is seen, calulate # it and append it to the stack
-# time | space : O(N)
-
-# ["17","+","5","+"]
-
-# 10 * (6 / (12  * -11)) + 17 + 5
-
-# ["5", "2", "3", "*", "+"]
-# [11 ]
+# Time and Space: O(N), all one time. Every time I see a (operand) o do 2 cosntant time op
+# max possible number of operands is < N
 class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:        
-        def calculations(a, b, op):
-            match op:
-                case "+":
-                    return a + b
-                case "-":
-                    return a - b
-                case "/":
-                    return int(a / b)
-                case "*":
-                    return a * b
-
-        operations =  set(["+", "-", "/", "*"])
-        # [0]
-        stack = []
-        # tokens = ["2", "3", "/"]
-        # c = "/"
-        for c in tokens:
-            if c not in operations:
-                stack.append(int(c))
-
-            elif len(stack) > 1:
-                # 3
-                sec = stack.pop(-1) 
-                # 2
-                first = stack.pop(-1) 
-                                
-                result = calculations(first, sec, c)
-                stack.append(result)
-            
-            else:
-                return None
-
-        if len(stack) != 1:
-            return None
+    def evalRPN(self, tokens: List[str]) -> int:
+        if len(tokens) < 3:
+            return int(tokens[0])
         
-        return stack[0]
+        def dfs():
+            cur_char = tokens.pop()
+            if cur_char not in "+-/*":
+                return int(cur_char)
+            
+            second = dfs()
+            first = dfs()
+
+            match cur_char:
+                case "+":
+                    return first + second
+                case "-":
+                    return first - second
+                case "/":
+                    return int(first / second)
+                case "*":
+                    return first * second
+
+
+        return dfs()
