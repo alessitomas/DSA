665750c5fd9139a63008e3306e4986d002cec3ee
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-06-01 17:52:27 -0300

    Time: 40 ms (32.88%) | Memory: 17.7 MB (58.64%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.py
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.py
@@ -26,46 +26,54 @@ class Solution:
     # i = 3 -> [0,0,1]
 
 
+# [0,0,0]               # [0, 1, 0]
+# [1,0,0]              # [0, 2, 0]
+# [2,0,0]
+# [3,0,0]
+
+            
+    def three_sum(self, target, possible_nums):
+        unique_combinations = 0
+        for i in range(len(possible_nums) - 2):
+            # no duplicates
+            if i > 0 and possible_nums[i] == possible_nums[i-1]:
+                continue
+            j = i + 1
+            k = len(possible_nums) - 1
+            matching = target - possible_nums[i]
+            while k > j:
+                cur_sum = possible_nums[j] + possible_nums[k]
+                if cur_sum < matching:
+                    j += 1
+                    # no duplicates
+                elif cur_sum > matching:
+                    k -= 1
+                else:
+                    if (possible_nums[i] == possible_nums[j] 
+                        and possible_nums[j] == possible_nums[k]):
+                        unique_combinations += 1
+                    elif (possible_nums[i] != possible_nums[j] 
+                        and possible_nums[j] != possible_nums[k]):
+                        unique_combinations += 6
+                    else:
+                        unique_combinations += 3
+                    
+                    prev_j = possible_nums[j]
+                    j += 1
+                    while j < k and prev_j == possible_nums[j]:
+                        j += 1
+                    k -= 1
+
+        return unique_combinations
     
-
     def distributeCandies(self, n: int, limit: int) -> int:
-        solutions = 0
-        def backtracking(state, candies_left, limit, par_idx):
-            nonlocal solutions
-            if candies_left == 0:
-                solutions += 1
-                state[par_idx] -= 1
-                return 
-            
-            possible_give_amount = limit * (3 - par_idx)
-            
-            for i in range(3):
-                if i >= par_idx:
-                    possible_give_amount -= state[i]
+        possible_nums = []
+        
+        for i in range(limit+1):
+            for _ in range(3):
+                possible_nums.append(i)
 
-            if candies_left > possible_give_amount:
-                state[par_idx] -= 1
-                return 
-
-            # backtracK ? 
-            # give to one
-            if state[0] <  limit and par_idx <= 0:
-                state[0] += 1 
-                backtracking(state, candies_left-1, limit, 0)
-            
-            # give to 2
-            if state[1] <  limit and par_idx <= 1:
-                state[1] += 1 
-                backtracking(state, candies_left-1, limit, 1)
-            
-            # give to 3
-            if state[2] <  limit and par_idx <= 2:
-                state[2] += 1 
-                backtracking(state, candies_left-1, limit, 2)
-            
-            state[par_idx] -= 1
-        
-        backtracking([0,0,0], n, limit, 0)
-        return solutions
-        
-        
+        combinations = self.three_sum(n, possible_nums)
+        # each combination can permute
+        # permutation of 3 chars: 3, 2, 1 -> 6
+        return combinations 
