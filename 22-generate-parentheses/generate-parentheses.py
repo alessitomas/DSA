4ccf3deeb9b5721285435b70f2b3386c936cfb46
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-06-01 15:28:04 -0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.31%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,50 @@
+# well formed
+# for every ) there must one ( before.
+
+
+# first solution
+# generate all possibles combinations
+# verify which one are valid
+
+
+
+# 3 
+# lenth = 6, ["(", ")"]
+# lenth = 4, ["(", ")"]
+
+# open_count = 0
+# if open_count is zero I need top open
+
+# lenth =  ["(", "(", ")"]
+# open_count = 1
+
+# lenght_left -cur_open  >= 2
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+           
+        solutions = []
+        start = ["("]
+        
+        def backtracking(cur_state, length_left, cur_open):
+            if length_left == 0:
+                copy = [p for p in cur_state]
+                solutions.append("".join(copy))
+            
+            else:
+                if cur_open == 0 or length_left - cur_open >= 2:
+                    cur_state.append("(")
+                    backtracking(cur_state, length_left-1, cur_open+1)
+                    
+                if cur_open > 0:
+                    cur_state.append(")")
+                    backtracking(cur_state, length_left-1, cur_open-1)
+            
+            # backtrack
+            cur_state.pop(-1)
+            return    
+
+                
+        backtracking(start, 2*n - 1, 1)
+        return solutions 
