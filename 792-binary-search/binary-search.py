bdb7b7b6f12f1b4a65a7cae0eacb173c1d49505a
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-06-03 08:56:40 -0300

    Time: 0 ms (100.00%) | Memory: 18.6 MB (73.83%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,21 +1,36 @@
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         # is nice to handle base case explicity even when my code already does that?
+#         i = 0
+#         j = len(nums) - 1
+
+#         while i <= j:
+#             mid = ((j - i) // 2) + i
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] > target:
+#                 j = mid - 1
+#             else:
+#                 i = mid + 1
+        
+#         return - 1
+        
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # is nice to handle base case explicity even when my code already does that?
-        i = 0
-        j = len(nums) - 1
-
-        while i <= j:
-            mid = ((j - i) // 2) + i
+        lp = 0
+        rp = len(nums) 
+        
+        while lp < rp:
+            mid = (lp + rp)  // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
-                j = mid - 1
+                rp = mid 
             else:
-                i = mid + 1
-        
-        return - 1
-        
-
+                lp = mid + 1
+        return -1
 
 
         
