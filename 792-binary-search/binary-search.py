514e4f72e45195e4f9d9dc190e425537973ca037
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-06-03 20:38:24 -0300

    Time: 0 ms (100.00%) | Memory: 18.7 MB (45.56%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,21 +1,27 @@
 class Solution:
+    # binary search each will take 
+    # time: O(log N), given that for each iteration we devide the search space N by half
+    # search space is all the sorted array
+    # I will use binary search to find the transition point (two values) between number smaller than target.
+    # to number greater or equal to target
     def search(self, nums: List[int], target: int) -> int:
-        # is nice to handle base case explicity even when my code already does that?
-        i = 0
-        j = len(nums) - 1
-
-        while i <= j:
-            mid = (j + i) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                j = mid - 1
+        l = 0
+        r = len(nums) - 1
+        
+        if len(nums) == 0:
+            return -1
+        
+        if nums[l] >= target or nums[r] < target:
+            if nums[l] == target:
+                return l
+            return -1
+        
+        while r - l  > 1:
+            mid = (r + l) // 2
+            if nums[mid] < target:
+                l = mid
             else:
-                i = mid + 1
+                r = mid
         
-        return - 1
-        
-
-
-
+        return r if nums[r] == target else -1
         
