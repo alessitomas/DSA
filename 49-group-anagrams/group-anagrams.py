57853ac8c26e79b53f148deffac29d84442ea319
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-08-16 10:54:41 -0300

    Time: 14 ms (51.21%) | Memory: 20.6 MB (76.53%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,36 @@
+"""
+
+anagram -> permutation of chars
+
+Solution 1:
+
+map: (sorted string) : group []
+
+
+for every word in strs
+    sort the word
+    if sorted word is in map add it to the group, else create a new group
+
+create a list out of the groups
+
+T: O(N L (log L) , where N is the length of the array and L is the largest string in the arr
+S: O(N * L)
+
+10 min
+
+"""
+
+
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anam_2_group = defaultdict(list)
+        
+        for w in strs:
+            sorted_w = "".join(sorted(w))
+            anam_2_group[sorted_w].append(w)
+            
+        return list(anam_2_group.values())
+            
+           
+        
