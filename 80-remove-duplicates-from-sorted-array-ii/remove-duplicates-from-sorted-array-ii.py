5e1da3760e93905ffea9745ac6cc65834fbce9f5
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-08-24 18:24:36 -0300

    Time: 85 ms (55.39%) | Memory: 20.4 MB (79.33%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -1,45 +1,68 @@
 """
 
-Solution 1: Perform a shift left has each duplicate that appear more than twice
+nums = [1,1,1,2,2,3]
+
+writer = 0
+seeker = 0
+
+Loop invariants:
+
+all values on the left of writer are sorted and with most two duplicates
+all values in the left of seeker were already written or should not be written
+
+exit condition: seeker == len(nums) that will lead to all values were already written or should not be written
+
+ 
+if seeker < 2 or nums[seeker] != nums[seeker - 2]:
+    write 
+    mv both
+
+mv seeker
 
 
 
+ 0 1 2 3 4 5
+[1,1,2,2,2,3]
+ 
+       w
+            s
 
 """
 
 
 class Solution:
-    # [1,1,2,2,3,4,5,5]
-    # []
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums: List[int]) -> int: 
+        writer = seeker = 0
         
-        # start overmapped
-        # end exclusive
-        def shift_left(start, arr):
-
-            for i in range(start, len(arr) -1 ):
-                arr[i] = arr[i+1]
-
-        cur_app = 1
-        removals = 0
-        idx = 1
-
-
-        while idx < len(nums) - removals:
-            if nums[idx] != nums[idx-1]:
-                idx += 1
-                cur_app = 1
+        cur_char = nums[seeker]
+        cur_count = 0
+    
+        while seeker < len(nums):
+            
+            if nums[seeker] == cur_char:
+                cur_count += 1
             else:
-                if cur_app == 1:
-                    cur_app += 1
-                    idx += 1
-                else:
-                    shift_left(idx, nums)
-                    removals += 1
-                    cur_app = 2
+                cur_char = nums[seeker]
+                cur_count = 1
 
-        return len(nums) - removals
 
+            write_seeker = seeker < 2 or 2 >= cur_count
+            if write_seeker:
+                nums[writer] = nums[seeker]
+                writer += 1
+            seeker += 1
+            
+        return writer
+                
+
+
+
+
+
+
+
+        
+  
 
 
 
