276dd7e8573572e8092c57a0c76b2f2e9933112c
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-09-02 14:38:12 -0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (7.80%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,106 +1,124 @@
-# a / b = 2
-# num / div = q 
+"""
 
-# num = q * div
-# {num : {div : 1 / q}}
-# num = q * div
-# {div : {num : 1 * q}}
+Solution 1 BFS:
 
-# {a : {b: 1 / 2}}
-# {b : {a: 2}}
-
-# b / c = 3
-
-# {b : {c: 1 / 3}}
-# {c : {b: 3}}
-
-# a / c
-# find a common key between a and c and divide else return -1
+T: O(E + Q * (E))
+S: O(E)
 
 
-# model as a graph problem
-# if there is a relation they must be connected
+Solution 2 Union-Find:
 
-# there are relations between division
+Union-Find aggregate connected components and is possible to store the relation between son and parent, using weights.
 
-# A / B = c
+T: O(E + Q * a(E)) ->O(E + Q)
+S: O(E)
 
-# A = B * c
-
-# A * C-> B
-# A = b * 2
-
-# B * C = A 
-
-# B / C -> A
+"""
 
 
-# A -> B is 1 / c 
-# B -> A is 1 * c 
-
-# A <-> B <-> C
-
-# A -> 
-
-# To calculate A / C, it is neede to traverse the relations graph from A to C calculating # the amount. If there is no connection return -1.
-
-# Space Complexity: (E + V + Q)
-# Time Complexity: (Q * (E + V))
-
-
-
-
-
-# identifier weight -> x
-# a -> 2 x
-# b - 3 x
-
-
-# a / b = 2/3
-
-# union(a,b)
-from collections import defaultdict
+from collections import deque
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        
-        graph = defaultdict(defaultdict)
-        
-        node_map = {}
-        
-        for (num, div), res in zip(equations, values):
-            # num -> div
-            graph[num][div] = res
-            # div -> num
-            graph[div][num] = 1/res
-            
-        results = []
-        
-        def dfs_traversal(cur_node, target_node, cur_amount, visited, NO_RESULT):
-            if cur_node in visited:
-                return NO_RESULT
-            if cur_node == target_node:
-                return cur_amount
-            
-            visited.add(cur_node)
-            
-            for neigh in graph[cur_node]:
-                weight = graph[cur_node][neigh]
-                res = dfs_traversal(neigh, target_node, cur_amount * weight, visited, NO_RESULT)
-                if res != NO_RESULT:
-                    return res
-            
-            return NO_RESULT
+        class DSU:
+            def __init__(self):
+                self.parent = dict()
+                self.weight = dict()
+                self.rank = dict()
 
-        NO_RESULT = None
-        for num, div in queries:
-            if num not in graph or div not in graph:
-                res = -1
-            else:
-                res = dfs_traversal(num, div, 1, set(), NO_RESULT)
-                res = \
-                    res if res is not NO_RESULT else -1
+            def init_sets(self, node):
+                if node not in self.parent:
+                    self.parent[node] = node
+                    self.weight[node] = 1.0
+                    self.rank[node] = 0
             
-            results.append(res)
+            def find(self, node):
+                # find root
+                if node not in self.parent:
+                    return 
 
-        return results
+                root = node
+                cum_ratio_to_root = 1.0
+                
+                while self.parent[root] != root:
+                    cum_ratio_to_root *= self.weight[root]
+                    root = self.parent[root]
 
+                # path compression
+                while node != root:
+                    next_parent = self.parent[node]
+                    self.parent[node] = root
+                    cur_weight = self.weight[node]
+                    self.weight[node] = cum_ratio_to_root
+                    cum_ratio_to_root /= cur_weight
+                    node = next_parent
+                
+                return root
+
+            def union(self, src, dst, val):
+            # (a/b) = val
+            # (a/root_a) = w[a]
+            # (root_a) = a / w[a]
+            # (b/root_b) = w[b]
+            # (root_b) = b / w[b]
+
+            # (a / w[a]) / (b / w[b]) -> (a * w[b] / w[a] * b). 
+            # (a/b) * w[b] / w[a]
+
+            # (root_a / root_b) = (a/b) * w[b] / w[a]
+            # (root_a / root_b) = val * w[b] / w[a]
+
+
+            # weight[root_x] = (x/y) * (y/root_y) / (x/root_x)
+        
+                root_src, root_dst = self.find(src), self.find(dst)
+                
+                if not root_src or not root_dst:
+                    return
+
+                rank_src, rank_dst = self.rank[root_src], self.rank[root_dst]
+                
+                if rank_src > rank_dst:
+                    self.parent[root_dst] = root_src
+                    self.weight[root_dst] = 1 / (val * self.weight[dst] / self.weight[src])
+                elif rank_src < rank_dst:
+                    # (root_a / root_b) = val * w[b] / w[a]
+                    self.parent[root_src] = root_dst
+                    self.weight[root_src] = val * self.weight[dst] / self.weight[src]
+                else: 
+                    self.parent[root_dst] = root_src
+                    self.weight[root_dst] = 1 / (val * self.weight[dst] / self.weight[src])
+                    self.rank[root_src] += 1
+            
+            def calculate_eq(self, src, dst):
+                root_src, root_dst = self.find(src), self.find(dst)
+                
+                if not root_src or not root_dst or root_src != root_dst:
+                    return -1.0 
+                
+                return self.weight[src] / self.weight[dst]
+
+        dsu = DSU()
+        for (src, dst), val in zip(equations, values):
+            dsu.init_sets(src)
+            dsu.init_sets(dst)
+
+            dsu.union(src, dst, val)
+
+        results = list()
+        
+        for q_src, q_dst in queries:
+            results.append(dsu.calculate_eq(q_src, q_dst))
+
+        return results 
+            
+
+
+        
+
+     
+            
+            
+            
+            
+            
+        
