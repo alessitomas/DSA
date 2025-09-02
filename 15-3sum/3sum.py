e47fd91b509554bfa273fbb9445ed5a4e6487a91
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-09-02 20:02:49 -0300

    Time: 1282 ms (11.93%) | Memory: 20.8 MB (36.09%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,15 +1,11 @@
 
 """
 
-Solution 2:
+Solution 2 clear and optimal:
 
 T: O(Nˆ2)
 S: O(N)
 
- 0 1 2
-[1,2,3]
-
-len = 3
 
 
 """
@@ -17,7 +13,7 @@ len = 3
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        triplets = set()
+        triplets = []
         nums.sort()
 
         for i in range(len(nums) - 2):
@@ -39,12 +35,12 @@ class Solution:
                 elif cur_sum < 0:
                     start += 1
                 else:
-                    triplets.add((nums[i], nums[start], nums[end]))
+                    triplets.append([nums[i], nums[start], nums[end]])
                     end -= 1
                     start += 1
 
 
-        return [ [t[0], t[1], t[2] ] for t in triplets]
+        return triplets
 
 
 
