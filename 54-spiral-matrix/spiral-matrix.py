5374be37278d83c990173693638b9acb341d02ae
alessitomas <104390020+alessitomas@users.noreply.github.com>
2025-09-05 07:20:29 -0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.68%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,30 @@
+class Solution:
+    def is_valid(visited, r, c):
+            return 0 <= r < len(visited) and 0 <= c < len(visited[0]) and visited[r][c] == 0
+
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return []
+        
+        visited = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
+        
+        cur_dir = 0
+        r, c = 0, 0
+        order = []
+
+        while len(matrix) * len(matrix[0]) > len(order):
+            order.append(matrix[r][c])
+            visited[r][c] = 1
+            dr, dc = directions[cur_dir]
+
+            if Solution.is_valid(visited, r + dr, c + dc):
+                r += dr
+                c += dc
+            else:
+                cur_dir = (cur_dir + 1) % len(directions)
+                dr, dc = directions[cur_dir]
+                r += dr
+                c += dc
+
+        return order
