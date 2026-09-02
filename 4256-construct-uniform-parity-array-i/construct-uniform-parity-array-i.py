c47a9c2d5eea44638332a4e4ad8209c161c7f87e
alessitomas <104390020+alessitomas@users.noreply.github.com>
2026-09-02 20:40:46 -0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (84.46%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,27 @@
+"""
+odd - odd = even:
+7 - 3 = 4
+
+
+even - even = even:
+
+6 - 4 = 2
+
+
+odd - even = odd:
+
+3 - 2 = 1
+
+
+even - odd = odd: 
+
+4 - 3 = 1
+
+"""
+
+
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True 
+        
