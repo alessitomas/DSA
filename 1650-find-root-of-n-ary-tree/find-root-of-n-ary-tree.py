7e60dd6df811e19ad813156f88b937f154336f4b
alessitomas <104390020+alessitomas@users.noreply.github.com>
2026-09-05 15:28:41 -0300

    Time: 97 ms (64.04%) | Memory: 31.6 MB (53.37%) - LeetSync

--- 1650-find-root-of-n-ary-tree/find-root-of-n-ary-tree.py
+++ 1650-find-root-of-n-ary-tree/find-root-of-n-ary-tree.py
@@ -13,26 +13,8 @@ root property
 1- Is ancestor of all nodes
 2- root does not have a parent
 
-
-visit all children nodes once, mark the only node that was no visited it must be the root! 
-
-N-ary tree has only reference to children and not to the parent 
-
-
-visiting all nodes once
-
-loop through tree O(N):
-
-dfs() if visited just return, if not explore
-
-total work here:
-
-if already visited O(1), 
-
-total work is O(N) + n * O(1) (from every parent trying to visit it again, or from the nood already visited on the list)
-
 time: O(N)
-space: O(N), keep track of visited in a set and call stack 
+space: O(N), keep track of visited in a set
 
 
 
@@ -40,18 +22,25 @@ space: O(N), keep track of visited in a set and call stack
 
 class Solution:
     def findRoot(self, tree: List['Node']) -> 'Node':
-        visited = set()
+        total_sum = 0
+        visited_sum = 0
 
         for node in tree:
             if node is None:
                 continue
             
+            total_sum += node.val
+            
             for c in node.children:
-                visited.add(c)
+                visited_sum += c.val
+                
             
         for node in tree:
-            if node not in visited:
-                return node
+            if node is None: 
+                continue
+            
+            if node.val == total_sum - visited_sum:
+                return node 
 
     
 
